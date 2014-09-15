c42ab0b21ebeac87f36689345efdac67aacec891
Kuniaki IGARASHI <igaiga@gmail.com>
2014-09-15 11:00:03 +0900

    Update books_app sample to rails4.1.6.

--- _sample_apps/books_app/db/schema.rb
+++ _sample_apps/books_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616231027) do
+ActiveRecord::Schema.define(version: 20140915015022) do
 
   create_table "books", force: true do |t|
     t.string   "title"
