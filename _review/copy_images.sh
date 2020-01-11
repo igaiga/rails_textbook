9496f2e1ec973f699c9b3cb39d6575fd571d56e4
Kuniaki Igarashi <igaiga@gmail.com>
2020-01-11 15:12:06 +0900

    Treat images

---
+++ _review/copy_images.sh
@@ -0,0 +1,7 @@
+cp -r ../assets/ images
+
+mv images/smallest-app images/02_smallest_app
+mv images/crud images/03_crud
+mv images/new-create images/04_new_create
+mv images/model images/05_model
+mv images/picture-upload images/07_picture_upload
