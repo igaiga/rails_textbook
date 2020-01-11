d169fc26ebfd05ffed5e13f317595f5771ba431b
Kuniaki Igarashi <igaiga@gmail.com>
2020-01-11 15:12:23 +0900

    Writing md_to_re

--- _review/md_to_re.sh
+++ _review/md_to_re.sh
@@ -1,12 +1,12 @@
 # $ gem i md2review
-md2review ../_posts/2014-04-01-preface.md > contents/chap01_preface.re
-md2review ../_posts/2014-06-01-smallest-app.md > contents/chap02_smallest_app.re
-md2review ../_posts/2014-07-01-crud.md > contents/chap03_crud.re
-md2review ../_posts/2014-08-01-new-create.md > contents/chap04_new_create.re
-md2review ../_posts/2014-09-01-model.md > contents/chap05_model.re
-md2review ../_posts/2016-01-11-gem.md > contents/chap06_gem.re
-md2review ../_posts/2016-01-12-picture-upload.md > contents/chap07_picture_upload.re
-md2review ../_posts/2016-02-17-postface.md > contents/chap08_postface.re
-# TODO: 変換後にreからヘッダ情報の除去が必要、現在は手作業。
-
+md2review ../_posts/2014-04-01-preface.md > contents/01_preface.re
+md2review ../_posts/2014-06-01-smallest-app.md > contents/02_smallest_app.re
+md2review ../_posts/2014-07-01-crud.md > contents/03_crud.re
+md2review ../_posts/2014-08-01-new-create.md > contents/04_new_create.re
+md2review ../_posts/2014-09-01-model.md > contents/05_model.re
+md2review ../_posts/2016-01-11-gem.md > contents/06_gem.re
+md2review ../_posts/2016-01-12-picture-upload.md > contents/07_picture_upload.re
+md2review ../_posts/2016-02-17-postface.md > contents/08_postface.re
+# TODO: 変換後にreファイルからヘッダ情報の除去が必要、現在は手作業。
+echo "reファイルからヘッダ情報を削除してください"
 
