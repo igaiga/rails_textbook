9daf5c0fb04cf3b1d1a9891695700b81432fafa4
takahashim <maki@rubycolor.org>
2020-03-05 22:53:24 +0900

    カバーもPDF内に取り込むように修正

--- _review/copy_images.sh
+++ _review/copy_images.sh
@@ -6,3 +6,4 @@ mv images/crud images/03_crud
 mv images/new-create images/04_new_create
 mv images/model images/05_model
 mv images/picture-upload images/07_picture_upload
+cp ../_figures/covers/rails_textbook_cover_omote.png images
