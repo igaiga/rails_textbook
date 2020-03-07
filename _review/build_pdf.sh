0acfbb05d47440fa64d97045213004091cf75643
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-07 13:17:30 +0900

    表紙の挿入を手動に戻す
    印刷版は表紙を入れないで作るためそちらに合わせる

--- _review/build_pdf.sh
+++ _review/build_pdf.sh
@@ -1,6 +1,5 @@
 # カラー版。ノンブルは入らない。表紙は自動で挿入されない。
 # 表紙、裏表紙は_figures/coversに置いてあるので、PDF作成後にmacのプレビューで画像を差し込む。
-# （config.ymlに読み込む設定をしてみたが入らなかった。詳しくは未調査。）
 ./copy_images.sh
 ./md_to_re.sh
 ./make_pdf_on_docker.sh
--- _review/build_pdf_for_print.sh
+++ _review/build_pdf_for_print.sh
@@ -1,3 +1,4 @@
+# モノクロ版。ノンブル入り。表紙は自動で挿入されない。
 ./copy_images.sh
 ruby change_images_gray.rb
 ./md_to_re.sh
